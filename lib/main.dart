607c9e5a2a921d7932c8ebdd80f1f3966a2f8bc2
Harley Mamalias <harleymamalias@gmail.com>
2023-09-05 11:45:18 +0800

    first color change in git

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'My First Activity - Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         ),
         home: MyHomePage(),
       ),
